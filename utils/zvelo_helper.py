7f5e98192698acf977a83769d3b3605c0fb3a543
minhnkb <nkbminh@gmail.com>
2020-08-28 15:50:04 +0700

    + Fix categories list

--- utils/zvelo_helper.py
+++ utils/zvelo_helper.py
@@ -4,14 +4,9 @@ from SiteFilterDB import *
 
 class ZveloHelper:
     def __init__(self, path, host, serial):
-        # url_debug(1)
         r = url_init(path, 0, host, serial);
         if r:
             print url_errstr(r)
-
-        # update()
-        # v4_map_id = url_get_mapping_id("zvelo-v4")
-        # rep_map_id = url_get_mapping_id("reputation")
         versions()
 
     def process_list_urls(self, urls):
@@ -22,25 +17,20 @@ class ZveloHelper:
         return results
 
     def lookup(self, url):
-        category0 = self.catstring(url_lookup(url))
-        category1 = self.catmatstring(url_lookup_match(url))
-        category2 = self.catmatstring(url_lookup_cache(url))
-        # category3 = self.catstring(url_lookup_map(url, rep_map_id))
-        # flags = url_flags_s(0)
-        # category4 = self.catmatstring(url_lookup_match_map_flags(url, rep_map_id, flags))
-        return category0, category1, category2
+        categories = self.catstring(url_lookup(url)).split("|")
+        while len(categories) < 3:
+            categories.append("")
+        return categories
 
     def catstring(self, x):
         if len(x) and x[0]:
             if x[0] < 0:
                 return url_errstr(x[0])
             else:
-                return url_categories_name(x, "| ")
+                return url_categories_name(x, "|")
         else:
             return "Uncategorized"
 
-    def catmatstring(self, x):
-        return x[-1] + ": " + self.catstring(x[:-1])
 
 if __name__ == '__main__':
     path = ""
